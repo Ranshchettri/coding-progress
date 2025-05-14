eb95cda0ab92e29c03d47d00a78d19e312b288b6
Ransh <ranshsunar@gmail.com>
2025-05-14 21:24:16 +0545

    practice new  question

---
+++ C-tution/function/Function_call.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+  int me(int,int)
+  void main()
+  {
+
+     int a=30,b=50;
+     printf("The value of A & B is %d \t%d" ,a,b);
+     me(a,b);   
+  }
+  
+
+  {
+    int me (int a, int b);
+    a=a+30;
+    b=b+20
+    printf("The vlaue of A & B is now %d\t%d",a,b);
+    return a;
+    return b;
+  }
+  
