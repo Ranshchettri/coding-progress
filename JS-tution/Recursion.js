a8e37b8cf8dce9c4902a8ec06419af63480a2520
Ransh <ranshsunar@gmail.com>
2025-06-13 00:11:04 +0545

    learning factrial and recursion

---
+++ JS-tution/Recursion.js
@@ -0,0 +1,30 @@
+// simple recursion test *******************
+
+function recursion() {
+  console.log("hello coder, how are you");
+  // recursion();
+}
+
+// recursion();
+
+
+
+// This function will print "hello world, how are you" 5 times
+// and then print "hello coder, how are you" once when num reaches 0.
+// The recursion will stop when num is 0, preventing an infinite loop.
+
+
+
+function recur(num) {
+  if (num == 3) {
+    console.log("hello coder, how are you");
+    return;
+  }
+
+  else {
+    console.log("hello wolrd, how are you");
+    recur(num - 3);
+
+  }
+}
+ recur(5); //5 is the initial value passed to the function.(num parameter)
